0630c787cc3ee22bd62f9a5492d9c66da2fa1106
smic29 <sibulo1991@gmail.com>
2024-02-19 18:10:35 +0800

    Replaced More button with icon

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,4 +2,5 @@ class Inquiry < ApplicationRecord
   enum inquiry_type: { import: 0, export: 1 }
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :inquiry_type, presence: true
 end
